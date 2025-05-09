683b24b7d01e61b9ef2f438c333424064f02a9a4
Marcille, Nicolas <nmarcille@ea.com>
2025-05-09 21:34:37 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import gradio as gr
+import time
+
+
+def interface_fun(Super_card_key):
+    time.sleep(3)
+    if Super_card_key == "Bear loves Tiger":
+        output_txt = f"Congrats! You won a night in [Jiva Hotel](https://www.jivahill.com/en/index.html)"
+    else:
+        output_txt = "You won nothing! Try again"
+    return output_txt
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("# Coop Birthday game ")
+    gr.Markdown("## Click below to see what you won for your birthday")
+
+    gr.Interface(fn=interface_fun, inputs="text", outputs=gr.Markdown())
+
+if __name__ == "__main__":
+    demo.launch()
